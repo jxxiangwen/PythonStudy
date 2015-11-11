648704df20c0e13c9a3bfc035eeb7bbeb0226172
zouxiangwen <jxxiangwen@live.com>
2015-11-11 14:34:29 +0800

    简单工厂模式和策略模式

---
+++ cn/edu/shu/design_mode/simple_factory.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'jxxia'
+
+"""
+    简单工厂模式
+"""
+
+
+class Operation(object):
+    def get_result(self, num1, num2):
+        pass
+
+
+class AddOperation(Operation):
+    def get_result(self, num1, num2):
+        return num1 + num2
+
+
+class SubOperation(Operation):
+    def get_result(self, num1, num2):
+        return num1 - num2
+
+
+class MulOperation(Operation):
+    def get_result(self, num1, num2):
+        return num1 * num2
+
+
+class DivOperation(Operation):
+    def get_result(self, num1, num2):
+        try:
+            result = num1 / num2
+        except:
+            return 0
+        return num1 / num2
+
+
+class OperationFactory(object):
+    def __init__(self, ):
+        self.operation = dict()
+        self.operation['+'] = AddOperation()
+        self.operation['-'] = SubOperation()
+        self.operation['*'] = MulOperation()
+        self.operation['/'] = DivOperation()
+
+    def create_operation(self, operation):
+        return self.operation[operation]
+
+
+if __name__ == '__main__':
+    print(OperationFactory().create_operation('+').get_result(1, 2))
+    print(OperationFactory().create_operation('/').get_result(1, 0))
+    assert (OperationFactory().create_operation('+').get_result(1, 2) == 3)
